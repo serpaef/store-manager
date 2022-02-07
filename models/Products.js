72be7de7491e7f349fd7a320ffcc851c10f5ff39
Fernando Serpa <serpaef@outlook.com>
2022-02-07 18:40:56 -0300

    fix: Corrige queries que estavam faltando ";"

--- models/Products.js
+++ models/Products.js
@@ -1,13 +1,13 @@
 const connection = require('./connection');
 
 async function getByName(name) {
-  const query = 'SELECT * FROM products WHERE name=?';
+  const query = 'SELECT * FROM products WHERE name = ?;';
   const result = await connection.execute(query, [name]);
   return result;
 }
 
 async function create(name, quantity) {
-  const query = 'INSERT INTO products (name, quantity) VALUES (?, ?)';
+  const query = 'INSERT INTO products (name, quantity) VALUES (?, ?);';
   await connection.execute(query, [name, quantity]);
   const result = await getByName(name);
   return result;
