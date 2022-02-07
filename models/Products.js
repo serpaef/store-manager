e53b0391415b1eae9ca852758e5d2b95a8306435
Fernando Serpa <serpaef@outlook.com>
2022-02-07 17:46:26 -0300

    feat: Faz o método create no modelo de produtos retornar o objeto criado.

--- models/Products.js
+++ models/Products.js
@@ -9,6 +9,8 @@ async function getByName(name) {
 async function create(name, quantity) {
   const query = 'INSERT INTO products (name, quantity) VALUES (?, ?)';
   await connection.execute(query, [name, quantity]);
+  const result = await getByName(name);
+  return result;
 }
 
 module.exports = {
