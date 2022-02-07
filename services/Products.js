200f4fcb5b89631193674b0917057c71b72688b1
Fernando Serpa <serpaef@outlook.com>
2022-02-07 17:53:55 -0300

    feat: Cria o serviço verifyName nos serviços de produto

--- services/Products.js
+++ services/Products.js
@@ -2,6 +2,17 @@ function sendError(status, message) {
   return { status, message };
 }
 
+function verifyName(name) {
+  switch (true) {
+    case !name: return sendError(400, '"name" is required');
+    case typeof name !== 'string': return sendError(422, '"name" must be a string');
+    case name.length < 5: return sendError(422, '"name" length must be at least 5 characters long');
+    default:
+      break;
+  }
+}
+
 module.exports = {
   sendError,
+  verifyName,
 };
