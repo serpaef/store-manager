46bf7c01a16909c8e87cf556b2849f6447433536
Fernando Serpa <serpaef@outlook.com>
2022-02-08 14:27:03 -0300

    fix: Corrige queries para inserção de dados no db

--- models/Sales.js
+++ models/Sales.js
@@ -1,24 +1,20 @@
 const connection = require('./connection');
 
 async function generateSale() {
-  const query = 'INSERT INTO sales (), VALUES();';
+  const query = 'INSERT INTO sales () VALUES ();';
   const [sale] = await connection.execute(query, []);
   return sale.insertId;
 }
 
 async function registerSale(saleId, products) {
-  const query = 'INSERT INTO sales_products (sale_id, product_id, quantity), VALUES ?;';
-  const values = products.map((product) => (
-    [
-      [saleId], [product.product_id], [product.quantity],
-    ]
-  ));
+  const query = 'INSERT INTO sales_products (sale_id, product_id, quantity) VALUES ?;';
+  const values = products.map((product) => ([[saleId], [product.product_id], [product.quantity]]));
   /*
     https://stackoverflow.com/questions/67672322/bulk-insert-with-mysql2-and-nodejs-throws-500
     o connection.execute não consegue fazer bulk-insertions no banco de dados,
     para isso fazemos uso do connection.query 
   */
-  await connection.query(query, values);
+  await connection.query(query, [values]);
 }
 
 async function getItemSoldBySaleId(id) {
