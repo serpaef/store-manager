09629a6f63649c6450876de644c6bd118dffd511
Fernando Serpa <serpaef@outlook.com>
2022-02-08 11:39:23 -0300

    feat: Cria método createSale no modelo de vendas
    Método que cria uma venda e retorna o id.

--- models/Sales.js
+++ models/Sales.js
@@ -1,5 +1,11 @@
 const connection = require('./connection');
 
+async function createSale() {
+  const query = 'INSERT INTO sales (), VALUES();';
+  const [sale] = await connection.execute(query, []);
+  return sale.insertId;
+}
+
 async function getAll() {
   const query = 'SELECT * FROM sales';
   const [list] = connection.execute(query, []);
@@ -8,4 +14,5 @@ async function getAll() {
 
 module.exports = {
   getAll,
+  createSale,
 };
