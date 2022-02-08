4f7a77a5be849e71536c5fa10edd45e206490493
Fernando Serpa <serpaef@outlook.com>
2022-02-08 13:47:59 -0300

    feat: Cria métodos necessários para inserir uma venda no banco de dados

--- models/Sales.js
+++ models/Sales.js
@@ -1,9 +1,39 @@
 const connection = require('./connection');
 
-async function createSale() {
+async function generateSale() {
   const query = 'INSERT INTO sales (), VALUES();';
   const [sale] = await connection.execute(query, []);
   return sale.insertId;
+}
+
+async function registerSale(saleId, products) {
+  const query = 'INSERT INTO sales_products (sale_id, product_id, quantity), VALUES ?;';
+  const values = products.map((product) => (
+    [
+      [saleId], [product.product_id], [product.quantity],
+    ]
+  ));
+  /*
+    https://stackoverflow.com/questions/67672322/bulk-insert-with-mysql2-and-nodejs-throws-500
+    o connection.execute não consegue fazer bulk-insertions no banco de dados,
+    para isso fazemos uso do connection.query 
+  */
+  await connection.query(query, values);
+}
+
+async function getItemSoldBySaleId(id) {
+  const query = 'SELECT * FROM sales_products WHERE sale_id = ?';
+  const [sales] = await connection.execute(query, [id]);
+  return sales;
+}
+
+async function create(sales) {
+  const id = await generateSale();
+  await registerSale(id, sales);
+
+  const itemsSold = await getItemSoldBySaleId(id);
+
+  return { id, itemsSold };
 }
 
 async function getAll() {
@@ -14,5 +44,7 @@ async function getAll() {
 
 module.exports = {
   getAll,
-  createSale,
+  generateSale,
+  registerSale,
+  create,
 };
