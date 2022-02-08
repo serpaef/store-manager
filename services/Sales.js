40f9cff5a7687efb76597e9f3f58df2ea0c0a882
Fernando Serpa <serpaef@outlook.com>
2022-02-08 14:00:43 -0300

    feat: Adiciona métodos de verificação de id e quantidade nos serviços.

--- services/Sales.js
+++ services/Sales.js
@@ -4,6 +4,26 @@ function sendError(status, message) {
   return { status, message };
 }
 
+function verifyProductId(sales) {
+  for (let i = 0; i < sales.length; i += 1) {
+    if (!sales[i].product_id) {
+      return sendError(400, '"product_id" is required');
+    }
+  }
+}
+
+function verifyProductQuantity(sale) {
+  if (!sale.quantity) {
+    return sendError(400, '"quantity" is required');
+  }
+  if (typeof sale.quantity !== 'number' || sale.quantity < 1) {
+    return sendError(
+      422,
+      '"quantity" must be a number larger than or equal to 1',
+    );
+  }
+}
+
 async function create(sales) {
   const sale = await Sales.create(sales);
   return sale;
@@ -11,5 +31,7 @@ async function create(sales) {
 
 module.exports = {
   sendError,
+  verifyProductId,
+  verifyProductQuantity,
   create,
 };
